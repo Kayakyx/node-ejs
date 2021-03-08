f798ff794266471f674daecd37c71011aa9a919e
Kayakyx <1522025433@qq.com>
2021-03-08 22:58:50 +0800

    ejs & spa demo

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ### ejs 模板引擎 练习
 
+*扩展知识*
+
+spa 原理 demo /views/spa-example/admin.html
 
 *附：后端 模板引擎 集成 库 [consolidate](https://www.npmjs.com/package/consolidate)*
--- examples/express-ejs.js
+++ examples/express-ejs.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 
 const PORT = 3000;
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.get('/foo', (req, res)=>{
     const data = {title: '欢迎！', isLogin: false, list: [1, 2, 3, 4, 5, 6]};
@@ -13,17 +13,30 @@ app.get('/foo', (req, res)=>{
         console.log('ejs.renderFile', str);
         res.send(str);
     })
-
 });
 
 app.get('/login', (req, res)=>{
-    const data = {title: '欢迎！', name: '孙悟空', isLogin: true, list: [1, 2, 3, 4, 5, 6]};
+    const data = {
+        title: '欢迎！',
+        name: '孙悟空',
+        isLogin: true,
+        list: [1, 2, 3, 4, 5, 6],
+        userHtml: '<h4>登录用户可见</h4>',
+        detail: {
+            id: 6006000,
+            content: '这里是详情 content'
+        }
+    }
     ejs.renderFile('../views/index.html', data, (err, str)=>{
         if(err) throw err;
         console.log('ejs.renderFile', str);
         res.send(str);
     })
 
+});
+
+app.get('/getDetail', (req, res)=>{
+    res.send('this is getDetail');
 });
 
 
