bf365db52fed444110030c4a4e6b52218ef4f268
Kayakyx <1522025433@qq.com>
2021-03-22 23:11:51 +0800

    res.send cb

--- express/index.js
+++ express/index.js
@@ -18,6 +18,28 @@ app.get('/', (req, res) => {
     res.render('index.html', {title: 'Hey', message: 'Hello Engine'})
 });
 
+// dome 片段
+app.get('/home', (req, res) => {
+    console.log('/home')
+    const user = {
+      name: 'zhangsan'
+    };
+    /*
+    如果我们想返回这样形式的 参数
+    { code: 200, msg: 'ok', data: dom 片段 }
+    可以制定 第三个 callback ，这样就不执行自动返回了， 需要手动
+    */
+    res.render('./fragment/home.html', {user}, (err, html) => {
+        // console.log('这里是什么', html)
+        // 手动返回
+        res.send({
+            cood: 200,
+            msg: 'ok',
+            data: html
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
 });
