124da7d8a1bc94845cd5f457b170f148d7bee085
Kayakyx <1522025433@qq.com>
2021-03-23 21:58:34 +0800

    use consolidate

---
+++ express/cons.js
@@ -0,0 +1,57 @@
+/**
+ * 通过模板引擎库 使用 ejs 模板引擎
+ */
+
+const express = require('express');
+const ejs = require('ejs');
+const cons = require('consolidate');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// // https://www.expressjs.com.cn/5x/api.html#app.set
+// // https://www.expressjs.com.cn/5x/api.html#app.engine
+// // 设置一下模板存放的地址
+// app.set('views', path.join(__dirname, 'views'));
+// // 设置一下模板的文件类型
+// app.set('view engine', 'html');
+// // 指定你使用的模板引擎
+// app.engine('html', ejs.__express);
+
+// assign the ejs engine to .html files
+app.engine('html', cons.ejs);
+// set .html as the default extension
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    console.log('访问 /')
+    res.render('index.html', {title: 'Hey', message: 'Hello Engine'})
+});
+
+// dome 片段
+app.get('/home', (req, res) => {
+    console.log('/home')
+    const user = {
+      name: 'zhangsan'
+    };
+    /*
+    如果我们想返回这样形式的 参数
+    { code: 200, msg: 'ok', data: dom 片段 }
+    可以制定 第三个 callback ，这样就不执行自动返回了， 需要手动
+    */
+    res.render('./fragment/home.html', {user}, (err, html) => {
+        // console.log('这里是什么', html)
+        // 手动返回
+        res.send({
+            cood: 200,
+            msg: 'ok',
+            data: html
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server listening at http://localhost:${port}`);
+});
+
